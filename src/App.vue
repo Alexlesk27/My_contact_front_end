3ee38d4931f7d3dd7eba772e120b6aecbb3ef28a
Alex Silva <a0690349@gmail.com>
2024-07-13 22:51:19 -0400

    add image to contacts

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,8 @@ const name = ref('');
 const email = ref('');
 const phone = ref('');
 const contacts = ref([]);
+const image = ref(null)
+const editContactId = ref()
 
 const errors = ref({
   name: '',
@@ -19,11 +21,12 @@ const showEditForm = ref(false);
 
 async function handleForm() {
   if (validateForm()) {
-    await client.post('/v1/contact', {
-      name: name.value,
-      email: email.value,
-      phone: phone.value
-    });
+    const form = new FormData;
+    form.append("name", name.value)
+    form.append("email", email.value)
+    form.append("phone", phone.value)
+    form.append("image", image.value)
+    await client.post('/v1/contact', form);
     listContacts();
     clearForm();
   }
@@ -55,6 +58,11 @@ function validateForm() {
   return isValid;
 }
 
+function saveImage(event) {
+  console.log(event)
+  image.value = event.target.files[0]
+}
+
 async function listContacts() {
   contacts.value = (await client.get('/v1/contact')).data;
 }
@@ -65,6 +73,7 @@ async function deleteContact(id) {
 }
 
 async function updateContact() {
+
   await client.put(`/v1/contact/${editContactId.value}`, {
     name: name.value,
     email: email.value,
@@ -82,6 +91,7 @@ function fillEditForm(contact) {
   phone.value = contact.phone;
   disableCreateForm.value = true;
   showEditForm.value = true;
+  editContactId.value = contact.id;
 }
 
 function clearForm() {
@@ -100,18 +110,22 @@ listContacts();
   <div class="container">
     <form @submit.prevent="handleForm" class="form">
       <div class="input-container">
-        <input v-model="name" type="text" placeholder="Informe seu nome" class="input" :disabled="disableCreateForm" :class="{ 'input-error': errors.name }">
+        <input v-model="name" type="text" placeholder="Informe seu nome" class="input" :disabled="disableCreateForm"
+          :class="{ 'input-error': errors.name }">
         <span v-if="errors.name" class="error-message">{{ errors.name }}</span>
       </div>
       <div class="input-container">
-        <input v-model="email" type="email" placeholder="Informe seu email" class="input" :disabled="disableCreateForm" :class="{ 'input-error': errors.email }">
+        <input v-model="email" type="email" placeholder="Informe seu email" class="input" :disabled="disableCreateForm"
+          :class="{ 'input-error': errors.email }">
         <span v-if="errors.email" class="error-message">{{ errors.email }}</span>
       </div>
       <div class="input-container">
-        <input v-model="phone" type="text" placeholder="Informe seu contato" class="input" :disabled="disableCreateForm" :class="{ 'input-error': errors.phone }">
+        <input v-model="phone" type="text" placeholder="Informe seu contato" class="input" :disabled="disableCreateForm"
+          :class="{ 'input-error': errors.phone }">
         <span v-if="errors.phone" class="error-message">{{ errors.phone }}</span>
       </div>
-      <input type="file" placeholder="Escolha uma imagem" class="input" :disabled="disableCreateForm">
+      <input type="file" @change="saveImage" placeholder="Escolha uma imagem" class="input"
+        :disabled="disableCreateForm">
       <button type="submit" class="button" :disabled="disableCreateForm">Salvar contato</button>
     </form>
   </div>
@@ -120,6 +134,7 @@ listContacts();
     <table v-if="contacts.length > 0">
       <thead>
         <tr>
+          <th></th>
           <th>Nome</th>
           <th>Email</th>
           <th>Telefone</th>
@@ -128,6 +143,7 @@ listContacts();
       </thead>
       <tbody>
         <tr v-for="contact in contacts" :key="contact.id">
+          <td><img style="width: 80px;" :src="contact.image" alt=""></td>
           <td>{{ contact.name }}</td>
           <td>{{ contact.email }}</td>
           <td>{{ contact.phone }}</td>
